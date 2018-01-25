fb92fa0b667af73baddd52c42109740a0a8a9a9d
estigmata <nestor.otondo@fundacion-jala.org>
2018-01-25 03:58:57 -0400

    Class bullet added

--- js/aircraft.js
+++ js/aircraft.js
@@ -1,4 +1,39 @@
+class Scenario {
+  constructor() {
+    this.maxXScreen = window.screen.width;
+    this.maxYScreen = window.screen.height;
+  }
+}
 
+class Bullet {
+  constructor(params) {
+    this.bullet = document.createElement('div');
+    this.bullet.classList.add('bullet');
+    this.left = params.x + 170;
+    this.top = params.y + 38;
+    document.body.appendChild(this.bullet);
+    this.bullet.style.left = this.left + 'px';
+    this.bullet.style.top = this.top + 'px';
+  }
+
+  burst(maxXScreen) {
+    var params = {
+      bullet: this.bullet,
+      left: this.left,
+      maxXScreen: maxXScreen
+    }
+    var line = setInterval(setFire, 0);
+    function setFire() {
+      if (params.left > params.maxXScreen) {
+        params.bullet.remove();
+        clearInterval(line);
+      } else {
+        params.left++;
+        params.bullet.style.left = params.left + 'px';
+      }
+    };
+  }
+}
 
 
 class AirCraft {
@@ -29,29 +64,13 @@ class AirCraft {
   }
 
   fire() {
-    this.player.style.animation = "fire .15s infinite";
-    this.burst();
-  }
-
-  burst() {
-    var bullet = document.createElement('div');
-    var left = this.x + 170;
-    var xScreen = window.screen.width - 26;
-    bullet.classList.add('bullet');
-    document.body.appendChild(bullet);
-    bullet.style.top = this.y + 38 + 'px';
-    bullet.style.left = left + 'px';
-    var line = setInterval(lineFire, 1);
-    function lineFire() {
-      if (left > xScreen) {
-        console.log('remove bullet');
-        bullet.remove()
-        clearInterval(line);
-      } else {
-        left++;
-        bullet.style.left = left + 'px';
-      }
-    }
+    this.player.style.animation = "fire 0s infinite";
+    var params = {
+      x: this.x,
+      y: this.y,
+    };
+    var bullet = new Bullet(params);
+    bullet.burst(window.screen.width - 26);
   }
 
   info() {
